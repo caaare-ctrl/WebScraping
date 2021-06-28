9e18d7038f7caf110ce0cf67c07c5e00471c829e
atris <atrisyeung@gmail.com>
2021-06-28 21:05:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+import csv
+import pandas as pd
+#contain a bunch of constant
+from selenium.webdriver.common.keys import Keys
+# it is shorter than using bs4 from import request -> make soup
+# bs4 is good for scraping html website but not good for site with js angular react...
+
+# webdriver can drive the chrome browser or other browswe like firefox
+chrome_driver_path = "C:\Development\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+scrap_url = "https://steamdb.info/"
+driver.get(scrap_url)
+
+tables = driver.find_elements_by_tag_name("table")
+for i in range(len(tables)):
+    all_data = []
+    files_name = ["most_played.csv","trending.csv","popular_releases.csv","hot_release.csv"]
+    names = tables[i].find_elements_by_tag_name("thead tr th")
+    column_name = [name.text for name in names]
+    print(column_name)
+    rows = tables[i].find_elements_by_tag_name("tr")
+
+    for row in rows:
+        columns = row.find_elements_by_tag_name("td")
+        steam_row = [data.text for data in columns if data.text]
+        if steam_row:
+            all_data.append(steam_row)
+        with open(files_name[i], "w",newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(column_name)
+            writer.writerows(all_data)
+
+driver.close()
+
+
+#
+# df = pd.read_csv("steam_most_played.csv")
+# print(df)
